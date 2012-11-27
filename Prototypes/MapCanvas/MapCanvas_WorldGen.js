7fb9a8db983ad695654fa8666e3d86b0c2f4698d
Alexander Gryson <agryson@gmail.com>
2012-11-28 00:45:26 +0100

    sublime and git

--- Prototypes/MapCanvas/MapCanvas_WorldGen.js
+++ Prototypes/MapCanvas/MapCanvas_WorldGen.js
@@ -243,8 +243,8 @@ MersenneTwister.prototype.genrand_res53 = function() {
  * You can pass in a random number generator object if you like.
  * It is assumed to have a random() method.
  */
-var ClassicalNoise = function(r) { // Classic Perlin noise in 3D, for comparison 
-    if(r == undefined) r = Math;
+var ClassicalNoise = function(r) { // Classic Perlin noise in 3D, for comparison
+    if(r === undefined) r = Math;
     this.grad3 = [
       [1, 1, 0],
       [-1, 1, 0],
@@ -263,10 +263,10 @@ var ClassicalNoise = function(r) { // Classic Perlin noise in 3D, for comparison
     for(var i = 0; i < 256; i++) {
       this.p[i] = Math.floor(r.random() * 256);
     }
-    // To remove the need for index wrapping, double the permutation table length 
+    // To remove the need for index wrapping, double the permutation table length
     this.perm = [];
-    for(var i = 0; i < 512; i++) {
-      this.perm[i] = this.p[i & 255];
+    for(var j = 0; j < 512; j++) {
+      this.perm[j] = this.p[j & 255];
     }
   };
 
@@ -282,23 +282,23 @@ ClassicalNoise.prototype.fade = function(t) {
   return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
 };
 
-// Classic Perlin noise, 3D version 
+// Classic Perlin noise, 3D version
 ClassicalNoise.prototype.noise = function(x, y, z) {
-  // Find unit grid cell containing point 
+  // Find unit grid cell containing point
   var X = Math.floor(x);
   var Y = Math.floor(y);
   var Z = Math.floor(z);
 
-  // Get relative xyz coordinates of point within that cell 
+  // Get relative xyz coordinates of point within that cell
   x = x - X;
   y = y - Y;
   z = z - Z;
 
-  // Wrap the integer cells at 255 (smaller integer period can be introduced here) 
+  // Wrap the integer cells at 255 (smaller integer period can be introduced here)
   X = X & 255;
   Y = Y & 255;
   Z = Z & 255;
-  // Calculate a set of eight hashed gradient indices 
+  // Calculate a set of eight hashed gradient indices
   var gi000 = this.perm[X + this.perm[Y + this.perm[Z]]] % 12;
   var gi001 = this.perm[X + this.perm[Y + this.perm[Z + 1]]] % 12;
   var gi010 = this.perm[X + this.perm[Y + 1 + this.perm[Z]]] % 12;
@@ -308,16 +308,16 @@ ClassicalNoise.prototype.noise = function(x, y, z) {
   var gi110 = this.perm[X + 1 + this.perm[Y + 1 + this.perm[Z]]] % 12;
   var gi111 = this.perm[X + 1 + this.perm[Y + 1 + this.perm[Z + 1]]] % 12;
 
-  // The gradients of each corner are now: 
-  // g000 = grad3[gi000]; 
-  // g001 = grad3[gi001]; 
-  // g010 = grad3[gi010]; 
-  // g011 = grad3[gi011]; 
-  // g100 = grad3[gi100]; 
-  // g101 = grad3[gi101]; 
-  // g110 = grad3[gi110]; 
-  // g111 = grad3[gi111]; 
-  // Calculate noise contributions from each of the eight corners 
+  // The gradients of each corner are now:
+  // g000 = grad3[gi000];
+  // g001 = grad3[gi001];
+  // g010 = grad3[gi010];
+  // g011 = grad3[gi011];
+  // g100 = grad3[gi100];
+  // g101 = grad3[gi101];
+  // g110 = grad3[gi110];
+  // g111 = grad3[gi111];
+  // Calculate noise contributions from each of the eight corners
   var n000 = this.dot(this.grad3[gi000], x, y, z);
   var n100 = this.dot(this.grad3[gi100], x - 1, y, z);
   var n010 = this.dot(this.grad3[gi010], x, y - 1, z);
@@ -326,19 +326,19 @@ ClassicalNoise.prototype.noise = function(x, y, z) {
   var n101 = this.dot(this.grad3[gi101], x - 1, y, z - 1);
   var n011 = this.dot(this.grad3[gi011], x, y - 1, z - 1);
   var n111 = this.dot(this.grad3[gi111], x - 1, y - 1, z - 1);
-  // Compute the fade curve value for each of x, y, z 
+  // Compute the fade curve value for each of x, y, z
   var u = this.fade(x);
   var v = this.fade(y);
   var w = this.fade(z);
-  // Interpolate along x the contributions from each of the corners 
+  // Interpolate along x the contributions from each of the corners
   var nx00 = this.mix(n000, n100, u);
   var nx01 = this.mix(n001, n101, u);
   var nx10 = this.mix(n010, n110, u);
   var nx11 = this.mix(n011, n111, u);
-  // Interpolate the four results along y 
+  // Interpolate the four results along y
   var nxy0 = this.mix(nx00, nx10, v);
   var nxy1 = this.mix(nx01, nx11, v);
-  // Interpolate the two last results along z 
+  // Interpolate the two last results along z
   var nxyz = this.mix(nxy0, nxy1, w);
 
   return nxyz;
