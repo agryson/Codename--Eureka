aec05540b139f91110214986959eabf8b9b5582a
Alexander Gryson <agryson@gmail.com>
2013-02-01 23:25:44 +0100

    Once more, Alex forgets to add fiels before pushing a commit :(

---
+++ CNE/strings/strings_en.js
@@ -0,0 +1,6 @@
+function Lang(){
+	this.noDig = "You can\'t dig here.";
+	this.noCavern = "You can\'t dig a cavern here.";
+	this.noDoze = "You can\'t prepare this terrain.";
+	this.noMine = "You can\'t mine here.";
+}
